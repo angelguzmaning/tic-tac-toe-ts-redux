5b3b074438dc04518dd8cb7c7684609e1a5323c0
Angel Guzman <angelguzmaning@gmail.com>
2021-08-02 22:57:32 -0400

    Rewrite Board to use two loops to make the squares

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,28 +1,22 @@
 import { BoardSquares, SquareValue } from '../../types/boardSquares';
 import { Square } from '../Square/Square';
+import * as R from 'ramda';
 
 export function Board({ squares, onCellClicked }: { squares: BoardSquares; onCellClicked: (index: number) => void }) {
   return (
     <div>
-      <div className='board-row'>
-        {renderSquare(squares[0], 0, onCellClicked)}
-        {renderSquare(squares[1], 1, onCellClicked)}
-        {renderSquare(squares[2], 2, onCellClicked)}
-      </div>
-      <div className='board-row'>
-        {renderSquare(squares[3], 3, onCellClicked)}
-        {renderSquare(squares[4], 4, onCellClicked)}
-        {renderSquare(squares[5], 5, onCellClicked)}
-      </div>
-      <div className='board-row'>
-        {renderSquare(squares[6], 6, onCellClicked)}
-        {renderSquare(squares[7], 7, onCellClicked)}
-        {renderSquare(squares[8], 8, onCellClicked)}
-      </div>
+      {R.range(0, 3).map((rowIndex) => (
+        <div className='board-row' key={rowIndex}>
+          {R.range(0, 3).map((columnIndex) => {
+            const index = rowIndex * 3 + columnIndex;
+            return renderSquare(squares[index], index, onCellClicked);
+          })}
+        </div>
+      ))}
     </div>
   );
 }
 
 function renderSquare(square: SquareValue, index: number, onCellClicked: (index: number) => void) {
-  return <Square value={square} onClick={() => onCellClicked(index)} />;
+  return <Square key={index} value={square} onClick={() => onCellClicked(index)} />;
 }
